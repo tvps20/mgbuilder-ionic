8e53f43105df74cc4586bb327b70d59705611b30
Santiago <thiago.v.santiago@accenture.com>
2020-08-22 14:00:42 -0300

    add card-ref service

--- src/app/shared/components/popover-card/popover-card.component.ts
+++ src/app/shared/components/popover-card/popover-card.component.ts
@@ -1,34 +1,50 @@
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 import { CardDTO } from './../../models/card.dto';
 import { DeckDTO } from './../../models/deck.dto';
 import { DeckService } from './../../services/domain/deck.service';
+import { CardRefService } from './../../services/domain/card-ref.service';
 
 @Component({
   selector: 'app-popover-card',
   templateUrl: './popover-card.component.html',
   styleUrls: ['./popover-card.component.scss'],
 })
-export class PopoverCardComponent implements OnInit {
+export class PopoverCardComponent implements OnInit, OnDestroy {
 
   @Input() card: CardDTO;
   public formulario: FormGroup;
   public decks$: Observable<DeckDTO[]>;
   public select$: Observable<string[]>;
+  private subscriptions$: Subscription[] = [];
 
   constructor(private formsBuider: FormBuilder,
+    private cardRefService: CardRefService,
     private deckService: DeckService) { }
 
   ngOnInit() {
     this.formulario = this.createForm();
     this.decks$ = this.deckService.findAll();
+    this.subscriptions$.push(this.selectChange());
+    this.select$ = this.cardRefService.getSelectCardRef(this.card.id);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach((subscription) => subscription.unsubscribe());
   }
 
   private createForm() {
     return this.formsBuider.group({
       add: [null]
+    });
+  }
+
+  private selectChange() {
+    return this.formulario.get('add').valueChanges.subscribe( success => {
+      let cardRef = this.cardRefService.parseToEntity(this.formulario, this.card);
+      this.subscriptions$.push(this.cardRefService.saveOrUpdate(cardRef).subscribe());
     });
   }
 }
---
+++ src/app/shared/services/domain/card-ref.service.ts
@@ -0,0 +1,141 @@
+import { FormGroup } from '@angular/forms';
+import { switchMap, map } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage'
+import { Observable } from 'rxjs';
+
+import { CardDTO } from './../../models/card.dto';
+import { CardRefDTO } from './../../models/card-ref.dto';
+import { CardRefType } from './../../util/enuns-type.enum';
+import { BdBaseCrudService } from '../../util/bd-base-crud';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CardRefService extends BdBaseCrudService<CardRefDTO> {
+
+    constructor(protected storage: Storage) {
+        super(storage, 'cardsRef')
+    }
+
+    public findAll(): Observable<CardRefDTO[]> {
+        return this.getStorage();
+    }
+
+    public saveOrRemove(obj: CardRefDTO, insert: boolean = true): Observable<any> {
+        return this.getStorage().pipe(
+            switchMap((cardsBd: CardRefDTO[]) => {
+                let index = cardsBd.findIndex(x => x.cardId === obj.cardId);
+
+                if (index != -1) {
+                    cardsBd.splice(index, 1);
+                }
+
+                cardsBd.push(obj);
+
+                return from(this.storage.set(this.storageKey, cardsBd));
+            })
+        );
+    }
+
+    public saveOrUpdate(cardRef: CardRefDTO) {
+        return this.getStorage().pipe(
+            switchMap((cardsBd: CardRefDTO[]) => {
+                let index = cardsBd.findIndex(x => x.cardId === cardRef.cardId);
+
+                if (index != -1) {
+                    cardsBd.splice(index, 1);
+                }
+
+                cardsBd.push(cardRef);
+
+                return from(this.storage.set(this.storageKey, cardsBd));
+            })
+        );
+    }
+
+    public findByCardId(cardId: string) {
+        return from(this.getStorage()).pipe(
+            map((cardsBd: CardRefDTO[]) => {
+                if (cardsBd) {
+                    let cardsRefList = cardsBd.filter(x => x.cardId === cardId);
+
+                    if (cardsRefList.length > 0) {
+                        return cardsRefList[0];
+                    }
+
+                    return null;
+                }
+            })
+        );
+    }
+
+    public getSelectCardRef(cardId: string) {
+        return this.findByCardId(cardId).pipe(
+            switchMap((cardRef: CardRefDTO) => {
+                let select: string[] = [];
+
+                if (cardRef) {
+                    if (cardRef.collection) {
+                        select.push(CardRefType.COLLECTION);
+                    }
+                    if (cardRef.favorites) {
+                        select.push(CardRefType.FAVORITE);
+                    }
+                    if (cardRef.wantList) {
+                        select.push(CardRefType.WANTLIST);
+                    }
+
+                    cardRef.decksIds.split(';').forEach(x => {
+                        if (x !== '') {
+                            select.push(x);
+                        }
+                    });
+                }
+
+                return of(select);
+            })
+        );
+    }
+
+    public parseToEntity(form: FormGroup, card: CardDTO): CardRefDTO {
+        let select: string[] = form.get('add').value || [];
+        let collection = false;
+        let favorites = false;
+        let wantList = false;
+        let decksIds: string = '';
+
+        select.forEach(x => {
+            switch (x) {
+                case (CardRefType.COLLECTION): {
+                    collection = true;
+                    break;
+                }
+                case (CardRefType.FAVORITE): {
+                    favorites = true;
+                    break;
+                }
+                case (CardRefType.WANTLIST): {
+                    wantList = true;
+                    break;
+                }
+                default: {
+                    decksIds = decksIds.concat(x + ';')
+                    break;
+                }
+            }
+        })
+
+
+        return {
+            created_at: new Date(),
+            update_at: new Date(),
+            cardId: card.id,
+            collection: collection,
+            favorites: favorites,
+            wantList: wantList,
+            decksIds: decksIds
+        };
+    }
+}
